c3a61bfccfb3dc30377eb4439c46375e09ce021a
Adam <ajjstepien@gmail.com>
2020-03-07 11:34:35 +0100

    Wszystko działa

---
+++ src/main/java/pl/sda/banki/BankAccount.java
@@ -0,0 +1,46 @@
+package pl.sda.banki;
+
+public class BankAccount {
+    private int id, accountNumber;
+    String name;
+    double balance;
+
+    public BankAccount( String name, int id, int accountNumber, double balance) {
+        this.id = id;
+        this.accountNumber = accountNumber;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
--- src/main/java/pl/sda/banki/Main.java
+++ src/main/java/pl/sda/banki/Main.java
@@ -3,6 +3,24 @@ package pl.sda.banki;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	User user1 = new User("Adam", "Stępień", 26);
+	User user2 = new User("Dominika", "Cieś", 21);
+	User user3 = new User("Daisy", "Dog", 12);
+	User user4 = new User("Roman", "Romański", 44);
+
+	BankAccount b1 = new BankAccount("BankDlaBogatych", 1, 5555, 6555);
+	BankAccount b2 = new BankAccount("BankNowoczesny", 2, 3242, 2523);
+	BankAccount b3 = new BankAccount("Bank", 3, 1005, 23);
+	BankAccount b4 = new BankAccount("BigBank", 4, 3528, 624);
+
+	user1.setBankAccount(b1);
+	user2.setBankAccount(b2);
+	user3.setBankAccount(b3);
+	user4.setBankAccount(b4);
+
+	user1.print();
+	user2.print();
+	user3.print();
+	user4.print();
     }
 }
